e61f853b207cc8f6f8c9562f4e90756bd0b7e997
JT <jerylltan@gmail.com>
2022-12-08 21:41:17 +0800

    completed 90% logic.

---
+++ app.js
@@ -0,0 +1,103 @@
+class Calculator {
+    constructor(previousOperandTextEle, currentOperandTextEle) {
+        this.previousOperandTextEle = previousOperandTextEle
+        this.currentOperandTextEle = currentOperandTextEle
+        this.clear()
+    }
+
+    clear() {
+        this.previous = ""
+        this.current = ""
+        this.operation = undefined
+        
+    }
+    
+    appendNumber(number) {
+        if (number === "." && this.current.includes('.')) return
+        this.current += number.toString()
+    }
+
+    selectOperation(operator) {
+        if(this.current === "") return
+        else if (this.previous !== "") {
+            this.compute()
+        }
+        this.operation = operator
+        this.previous = this.current
+        this.previous += " " + this.operation
+        this.current = ""
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previous)
+        const current = parseFloat(this.current)
+        if (isNaN(prev) || isNaN(current)) return
+        switch(this.operation) {
+            case "+":
+                computation = prev + current
+                break
+            case "-":
+                computation = prev - current
+                break
+            case "×":
+                computation = prev * current
+                break
+            case "÷":
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.current = computation
+        this.operation = undefined
+        this.previous = ""
+    }
+
+    getDisplayNumber(num) {
+        return num
+    }
+
+    updateDisplay() {
+        this.previousOperandTextEle.textContent = this.getDisplayNumber(this.previous)
+        this.currentOperandTextEle.textContent = this.getDisplayNumber(this.current)
+        
+    }
+
+}
+
+
+
+const nums = document.querySelectorAll('[data-number]')
+const ops = document.querySelectorAll('[data-operation]')
+const previous = document.querySelector('[data-previous]')
+const current = document.querySelector('[data-current]')
+const clear = document.querySelector('[data-clear]')
+const percent = document.querySelector('[data-percent]')
+const equal = document.querySelector('[data-equals]')
+
+const calculator = new Calculator(previous, current)
+
+nums.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.appendNumber(button.textContent)
+        calculator.updateDisplay()
+    })
+})
+
+clear.addEventListener("click", () =>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+ops.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.selectOperation(button.textContent)
+        calculator.updateDisplay()
+    })
+})
+
+equal.addEventListener("click", () =>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
